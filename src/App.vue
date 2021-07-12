f313740cf9f0ed3ed68fce8e0f716a6f8fdc067f
apolkingg8 <apolkingg8@gmail.com>
2021-07-12 21:44:43 +0800

    :tada: switch to vue2

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue";
+import {Component, Vue} from "vue-property-decorator";
+import HelloWorld from "./components/HelloWorld.vue";
 
-export default defineComponent({
-  name: "App",
+@Component({
   components: {
     HelloWorld,
   },
-});
+})
+export default class App extends Vue {}
 </script>
 
 <style>
-html,
-body {
-  background: #000;
-}
-
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
